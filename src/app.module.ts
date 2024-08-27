f42042ccf7c2411f3e307935a9d846eef51dd9c5
davigottt <97664310+davigottt@users.noreply.github.com>
2024-08-27 13:52:03 +0000

    feat: :sparkles: .env support for port configuration
    adding config service which uses enviroment variables and .env file for configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { DataModule } from './data/data.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Customer } from './data/data.entity';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
@@ -13,6 +15,11 @@ import { Customer } from './data/data.entity';
       database: 'db',
       synchronize: true,
       entities: [Customer],
+    }),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        PORT: Joi.number().port().default(3000),
+      }),
     }),
   ],
   controllers: [AppController],
