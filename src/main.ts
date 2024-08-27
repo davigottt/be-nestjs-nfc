74bbb30f5bd02d35ddd355bdb6f175c391258f3f
davigottt <97664310+davigottt@users.noreply.github.com>
2024-08-27 14:05:18 +0000

    feat(docs): :sparkles: openapi integration
    adding swagger module

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('My app')
+    .setDescription('this is my app')
+    .setVersion('1.0')
+    .addTag('app')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(configService.get('PORT'));
 }
