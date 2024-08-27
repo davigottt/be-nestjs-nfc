671776044cfc911042a804fd69eea13d276de5e8
davigottt <97664310+davigottt@users.noreply.github.com>
2024-08-27 16:46:24 +0000

    fix: :bug: limit customer properties - text length,etc..
    Limit properties via validation

--- src/data-create.dto.ts
+++ src/data-create.dto.ts
@@ -1,14 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, Length } from 'class-validator';
 
 export class PersonCreateDto {
   @ApiProperty({ description: 'The name of the customer', required: true })
   @IsNotEmpty()
+  @Length(3, 50)
   name: string;
   @ApiProperty({ description: 'The phone of the customer', required: true })
+  @Length(3, 20)
   @IsNotEmpty()
   phone: string;
   @ApiProperty({ description: 'The location of the customer', required: true })
   @IsNotEmpty()
+  @Length(3, 200)
   location: string;
 }
--- src/data-edit.dto.ts
+++ src/data-edit.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsString, Length } from 'class-validator';
 
 /** This is intented so you only need to pass the properties which you want to change.
  * The existing ones will stay the same.
@@ -7,11 +7,14 @@ import { IsString } from 'class-validator';
 export class PersonEditDto {
   @ApiProperty({ description: 'The name of the customer', required: false })
   @IsString()
+  @Length(3, 50)
   name: string;
   @ApiProperty({ description: 'The phone of the customer', required: false })
   @IsString()
+  @Length(3, 20)
   phone: string;
   @ApiProperty({ description: 'The location of the customer', required: false })
   @IsString()
+  @Length(3, 200)
   location: string;
 }
